c2258a74fd4ed559f4e08fa7d07273b9df5e21b0
oleg-divine <oleg.web.pro@gmail.com>
2020-07-06 17:20:53 +0300

    create lara + plugins

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-07-04 16:04:46.
+    * Generated on 2020-07-05 21:10:07.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.18.0 on 2020-07-04 16:04:45.
+ * Generated for Laravel 7.18.0 on 2020-07-05 21:10:07.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
