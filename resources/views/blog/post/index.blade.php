e1d5d3eb85625f23d32c73915367318aec99e46f
oleg-divine <oleg.web.pro@gmail.com>
2020-07-09 00:04:42 +0300

    add admin category controller and routes

--- resources/views/blog/post/index.blade.php
+++ resources/views/blog/post/index.blade.php
@@ -1,9 +1,14 @@
-<table>
-    @foreach($items as $item)
-        <tr>
-            <td>{{ $item->id }}</td>
-            <td>{{ $item->title }}</td>
-            <td>{{ $item->created_at }}</td>
-        </tr>
-    @endforeach
-</table>
+@extends('layouts.app')
+
+@section('content')
+
+    <table>
+        @foreach($items as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->title }}</td>
+                <td>{{ $item->created_at }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,32 @@ Route::get('/', function () {
 });
 
 
-Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function(){
-    Route::resource('posts', 'PostController')->names('blog.posts');
-});
-
-Route::resource('rest', 'RestTestController')->names('restTest');
+//Route::resource('rest', 'RestTestController')->names('restTest');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+// Admin panel
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog'
+];
+
+Route::group($groupData, function () {
+    // BlogCategory
+    $methods = [
+        'index',
+        'edit',
+        'store',
+        'update',
+        'create'
+    ];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
